6be9881f2db21351b1850c7b1288f790450659b1
hdeep03 <35414693+hdeep03@users.noreply.github.com>
2023-02-18 08:09:57 -0800

    Added Supplemental Videos API Endpoint

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 import asyncio
 from utils import download, transcribe
 from search import SearchHandler
+from videos import get_videos
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -22,25 +23,35 @@ class VideoID(BaseModel):
 class SearchQuery(BaseModel):
     id: str
     query: str
+class SuppVideoQuery(BaseModel):
+    id: str
+    timestamp: str
 
 search_handler = SearchHandler()
 
 @app.post("/api/v0/search")
-async def search(sq: SearchQuery):
+def search(sq: SearchQuery):
     video_id = sq.id
     query = sq.query
     starts, contexts = search_handler.search(query, video_id)
     return {"starts": starts, "contexts": contexts}
-    
-
 
 @app.post("/api/v0/process")
-async def process(video: VideoID):
+def process(video: VideoID):
     video_id = video.id
     path = download(video_id)                       # Downloads a youtube video
     transcript = transcribe(video_id)               # Transcribes the video
     return {"Transcript Status": "Success"}
 
+@app.post("/api/v0/videos")
+def videos(svq: SuppVideoQuery):
+    video_id = svq.id
+    time = int(svq.timestamp)
+    start_time = max(0, time-240)
+    urls = get_videos(start_time, time, video_id)
+    return {"urls": urls}
+
+    
 
 
 
---
+++ server/videos.py
@@ -0,0 +1,48 @@
+import numpy as np
+from utils import load_transcript
+import yake
+from youtubesearchpython import VideosSearch
+import string
+
+def findStart(start, transcript):
+    if start == 0:
+        return 0
+    for i, seg_start in enumerate(transcript['starts']):
+        if seg_start > start:
+            return i - 1
+    return len(transcript['starts']) - 1
+
+def findEnd(end, transcript):
+    for i, seg_end in enumerate(transcript['ends']):
+        if seg_end > end:
+            return i
+    return len(transcript['starts']) - 1
+
+def getSnippet(start, end, video_id):
+    transcript = load_transcript(video_id)
+    start_idx = findStart(start, transcript)
+    end_idx = findEnd(end, transcript)
+    snippet = " ".join(transcript['text'][start_idx:end_idx])
+    return snippet
+
+def extract_keywords(text):
+    kw_extractor = yake.KeywordExtractor()
+    keywords = kw_extractor.extract_keywords(text)
+    return keywords
+
+def get_videos(start, end, video_id, k=3):
+    snippet = getSnippet(start, end, video_id).strip()
+    snippet = "".join(x for x in snippet if x in string.printable)
+    print(snippet)
+    keywords = extract_keywords(snippet)
+    print(keywords)
+    query = " ".join([keywords[x][0] for x in range(3)])
+    videosSearch = VideosSearch(query, limit = 10)
+    d = videosSearch.result()
+    urls = []
+    for item in d['result']:
+        urls.append(item['id'])
+        print("https://www.youtube.com/embed/" + item['id'])
+    return urls
+
+
