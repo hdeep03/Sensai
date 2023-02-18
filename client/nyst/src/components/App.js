0675a6d45f0a7dc71c3811b335baf88711aed979
Abhay Bestrapalli <abhaybestx@gmail.com>
2023-02-17 23:55:01 -0800

    front end works

---
+++ client/nyst/src/components/App.js
@@ -0,0 +1,11 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import "../utilities.css";
+
+import { get, post } from "../utilities.js";
+
+function App() {
+  return <div className="App">hello!</div>;
+}
+
+export default App;
